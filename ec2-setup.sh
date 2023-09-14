988f91033733fd1f09ebe04d1fd55ca0b5a0561d
Shawn Sun <32376495+ssz1997@users.noreply.github.com>
2023-09-14 12:46:58 -0700

    Add files via upload

---
+++ ec2-setup.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+sudo yum -y install terraform
+
+ssh-keygen -t rsa -N '' -f ~/.ssh/id_rsa
+
+sudo yum install -y wget
+wget https://get.helm.sh/helm-v3.6.0-linux-amd64.tar.gz
+tar -xzf helm-v3.6.0-linux-amd64.tar.gz
+sudo mv linux-amd64/helm /usr/local/bin/
+rm -rf helm-v3.6.0-linux-amd64.tar.gz linux-amd64
+
+sudo curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod 755 kubectl
+sudo mv kubectl /usr/local/bin/
+
+sudo yum install docker
+sudo usermod -a -G docker ec2-user
+newgrp docker
+sudo systemctl enable docker.service
+sudo systemctl start docker.service
+
